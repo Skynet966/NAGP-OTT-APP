177f55e4b7e1691dad7c53c8fbdac5531bea3c50
Satish Yadav <satish.yadav@nagarro.com>
2022-07-05 14:20:08 +0530

    Initial app setup with side-nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SideNavComponent } from './layout/side-nav/side-nav.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SearchComponent } from './layout/header/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    SideNavComponent,
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
